eff41faa93f0d04f00d08e8c1d7c94c23068749c
lucianobajr <luciano.alcantara@ufv.br>
2023-05-16 13:46:34 -0300

    add

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,11 @@
 import * as vscode from "vscode";
+// import * as path from "path";
 
 export function activate(context: vscode.ExtensionContext) {
   console.log('Congratulations, your extension "endurance" is now active!');
+
+  const extensionUri = context.extensionUri;
+  const iconPath = joinPath(extensionUri, "icons", "endurance.svg");
 
   const fileIconTheme = vscode.extensions.getExtension("vscode.file-icons");
 
@@ -9,11 +13,11 @@ export function activate(context: vscode.ExtensionContext) {
     fileIconTheme.activate().then(() => {
       vscode.workspace.onDidChangeConfiguration((event) => {
         if (event.affectsConfiguration("files.associations")) {
-          updateFileIconAssociations();
+          updateFileIconAssociations(iconPath);
         }
       });
 
-      updateFileIconAssociations();
+      updateFileIconAssociations(iconPath);
     });
   }
 
@@ -26,16 +30,32 @@ export function activate(context: vscode.ExtensionContext) {
     }
   );
 
-  context.subscriptions.push(disposable);
+  // Create the status bar item
+  const statusBarItem = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Left
+  );
+  statusBarItem.tooltip = "Endurance Extension";
+  statusBarItem.color = new vscode.ThemeColor("statusBar.foreground");
+  statusBarItem.show();
+
+  statusBarItem.text = `$(${iconPath}) Endurance`;
+  statusBarItem.show();
+
+  context.subscriptions.push(disposable, statusBarItem);
 }
 
-function updateFileIconAssociations() {
+function updateFileIconAssociations(iconPath: string) {
   const config = vscode.workspace.getConfiguration("files");
   const fileAssociations = config.get<any>("associations") || {};
 
-  fileAssociations[".end"] = "icons/endurance.svg";
+  fileAssociations[".end"] = iconPath;
+  fileAssociations[".customext"] = iconPath;
 
   config.update("associations", fileAssociations, true);
+}
+
+function joinPath(uri: vscode.Uri, ...pathFragment: string[]): string {
+  return vscode.Uri.joinPath(uri, ...pathFragment).fsPath;
 }
 
 export function deactivate() {}
