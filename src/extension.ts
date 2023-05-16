2abff7365bfde35621c6c538711f0cf77f85ca3f
lucianobajr <luciano.alcantara@ufv.br>
2023-05-16 16:29:12 -0300

    add

--- src/extension.ts
+++ src/extension.ts
@@ -12,7 +12,20 @@ export function activate(context: vscode.ExtensionContext) {
     }
   );
 
+  const fileAssociations = vscode.workspace.getConfiguration('files').get<any>('associations') || {};
+  fileAssociations['.end'] = 'endurance';
+  vscode.workspace.getConfiguration('files').update('associations', fileAssociations, true);
+
+  // Register the configuration for Material Icons
+  const materialIconsConfig = vscode.workspace.getConfiguration('material-icon-theme');
+  const fileExtensionsConfig: { [key: string]: string } = materialIconsConfig.get('fileExtensions') || {};
+  fileExtensionsConfig['.end'] = 'icons/endurance.svg';
+  materialIconsConfig.update('fileExtensions', fileExtensionsConfig, true);
+
+  // Display a message to the user indicating the icon has been configured
+  vscode.window.showInformationMessage('Endurance icon has been set for .end files.');
+
   context.subscriptions.push(disposable);
 }
 
-export function deactivate() {}
+export function deactivate() { }
